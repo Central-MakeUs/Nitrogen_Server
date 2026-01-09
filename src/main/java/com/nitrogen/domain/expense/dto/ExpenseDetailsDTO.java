91bc7a061b9a295978296eee8f0cddfa00dc6722
huggywuggy1289 <153150703+huggywuggy1289@users.noreply.github.com>
2026-01-09 12:19:51 +0900

    :sparkles: Feature 예외처리를 위한 dto검증 명시 어노테이션 추가

--- src/main/java/com/nitrogen/domain/expense/dto/ExpenseDetailsDTO.java
+++ src/main/java/com/nitrogen/domain/expense/dto/ExpenseDetailsDTO.java
@@ -1,6 +1,8 @@
 package com.nitrogen.domain.expense.dto;
 
 import com.nitrogen.domain.expense.entity.enums.EmotionType;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -13,9 +15,18 @@ import java.time.LocalDate;
 @Builder
 @AllArgsConstructor
 public class ExpenseDetailsDTO {
-    private int amount;
+    @NotNull(message = "지출 금액은 필수 입력 사항입니다.")
+    @Positive(message = "지출 금액은 0보다 커야 합니다.")
+    private Integer amount;
+
+    @NotNull(message = "지출 일자는 필수 입력 사항입니다.")
     private LocalDate expendedAt;
+
+    @NotNull(message = "상위 카테고리는 필수 선택 사항입니다.")
     private Long categoryId;
+
     private Long subCategoryId;
+
+    @NotNull(message = "소비 감정은 필수 선택 사항입니다.")
     private EmotionType emotionType;
 }
