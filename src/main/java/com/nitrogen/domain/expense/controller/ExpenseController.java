4c0ee9f34ea4b5686cdc55a679eee8e0eece5482
huggywuggy1289 <153150703+huggywuggy1289@users.noreply.github.com>
2026-01-10 18:40:45 +0900

    :sparkles: Feature 지출 기록 회고(만족도 조사) API 구현 및 보안 구조 개선

--- src/main/java/com/nitrogen/domain/expense/controller/ExpenseController.java
+++ src/main/java/com/nitrogen/domain/expense/controller/ExpenseController.java
@@ -1,6 +1,7 @@
 package com.nitrogen.domain.expense.controller;
 
 import com.nitrogen.domain.expense.dto.ExpenseDetailsDTO;
+import com.nitrogen.domain.expense.dto.ExpenseRemindRequestDTO;
 import com.nitrogen.domain.expense.service.record.ExpenseService;
 import com.nitrogen.global.apiPayload.ApiResponse;
 import io.swagger.v3.oas.annotations.Operation;
@@ -8,6 +9,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
@@ -23,9 +25,17 @@ public class ExpenseController {
     @PostMapping("/record")
     public ApiResponse<Long> registerExpense(
             @RequestBody @Valid ExpenseDetailsDTO dto,
-            @RequestParam("userId") Long userId){
+            @AuthenticationPrincipal Long userId){
         Long savedExpenseId = expenseService.registerExpense(dto, userId);
         return ApiResponse.onSuccess(savedExpenseId);
+    }
+
+    // 소비기록 회고
+    @Operation(summary = "소비기록 회고", description = "유저가 하루일과동안 쓴 지출기록에 소비회고를 추가적으로 남깁니다.")
+    @PatchMapping("/remind")
+    public ApiResponse<Long> remindExpense(@RequestBody ExpenseRemindRequestDTO dto, @AuthenticationPrincipal Long userId) {
+        Long updatedId = expenseService.remindExpense(dto, userId);
+        return ApiResponse.onSuccess(updatedId);
     }
     
     // 지출기록 조회
