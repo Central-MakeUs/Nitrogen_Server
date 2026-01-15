8771481c07b8e572a7c184959315815a62253cc6
huggywuggy1289 <153150703+huggywuggy1289@users.noreply.github.com>
2026-01-15 16:53:13 +0900

    :recycle: Refactor 쿼리 오류 수정

--- src/main/java/com/nitrogen/domain/expense/repository/ExpenseRepository.java
+++ src/main/java/com/nitrogen/domain/expense/repository/ExpenseRepository.java
@@ -12,7 +12,7 @@ import java.util.List;
 public interface ExpenseRepository extends JpaRepository<Expense, Long> {
 
     // 월별 총액을 위한 계산
-    @Query("SELECT COALESCE(SUM(e.amount), 0) FROM Expense e WHERE e.user.id = :userId AND e.expendedAt BETWEEN :start AND :end")
+    @Query("SELECT COALESCE(SUM(e.amount), 0) FROM Expense e WHERE e.user.userId = :userId AND e.expendedAt BETWEEN :start AND :end")
     long calculateMonthlyTotal(@Param("userId") Long userId, @Param("start") LocalDate start, @Param("end") LocalDate end);
     List<Expense> findAllByUserIdAndExpendedAt(Long userId, LocalDate expendedAt);
 }
