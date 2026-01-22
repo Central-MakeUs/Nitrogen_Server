c5575c443df849483b19fa1c96dc71ea6a2a9bf2
huggywuggy1289 <153150703+huggywuggy1289@users.noreply.github.com>
2026-01-22 20:42:49 +0900

    :art: Style 주석제거

--- src/main/java/com/nitrogen/domain/expense/dto/report/MonthlyReportSummaryResponse.java
+++ src/main/java/com/nitrogen/domain/expense/dto/report/MonthlyReportSummaryResponse.java
@@ -1,7 +1,7 @@
 package com.nitrogen.domain.expense.dto.report;
 
 public record MonthlyReportSummaryResponse(
-        String month,            // 2025년 1월
-        long totalAmount,        // 월 총금액
-        boolean isOpened         // 오픈가능여부
+        String month,
+        long totalAmount,
+        boolean isOpened
 ) {}
--- src/main/java/com/nitrogen/domain/expense/dto/report/WeeklyReportResponse.java
+++ src/main/java/com/nitrogen/domain/expense/dto/report/WeeklyReportResponse.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public record WeeklyReportResponse(
         String weekRange,
-        long weeklyTotalAmount,        // 주차 총액
-        EmotionSummary topEmotion,     // 가장 많이 지출한 감정정보
-        List<EmotionSummary> emotionDetails // 금액 내림차순
+        long weeklyTotalAmount,
+        EmotionSummary topEmotion,
+        List<EmotionSummary> emotionDetails
 ) {}
