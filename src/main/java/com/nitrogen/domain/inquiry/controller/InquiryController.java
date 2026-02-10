e166d79c427a46d200d74066b747d814063fb635
huggywuggy1289 <153150703+huggywuggy1289@users.noreply.github.com>
2026-02-11 08:55:41 +0900

    :bug: Fix: 1:1문의 응답 형식 통일

--- src/main/java/com/nitrogen/domain/inquiry/controller/InquiryController.java
+++ src/main/java/com/nitrogen/domain/inquiry/controller/InquiryController.java
@@ -1,6 +1,7 @@
 package com.nitrogen.domain.inquiry.controller;
 
 import com.nitrogen.domain.inquiry.entity.InquiryRequest;
+import com.nitrogen.global.apiPayload.ApiResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
@@ -21,14 +22,14 @@ public class InquiryController {
     private final JavaMailSender mailSender;
 
     @PostMapping("/send")
-    @Operation(summary = "1:1 문의 메일 발송")
-    public ResponseEntity<String> sendInquiry(@RequestBody InquiryRequest request) {
+    @Operation(summary = "1:1 문의 메일 발송", description = "사용자가 작성한 문의 내용을 관리자 이메일로 전송합니다.")
+    public ApiResponse<String> sendInquiry(@RequestBody InquiryRequest request) {
         SimpleMailMessage message = new SimpleMailMessage();
         message.setTo("nitrogencharger@gmail.com"); // 관리자 이메일
         message.setText("작성자: " + request.userEmail() + "\n\n문의 내용:\n" + request.content());
 
         mailSender.send(message);
 
-        return ResponseEntity.ok("문의가 메일로 전달되었습니다.");
+        return ApiResponse.onSuccess("문의가 메일로 전달되었습니다.");
     }
 }
