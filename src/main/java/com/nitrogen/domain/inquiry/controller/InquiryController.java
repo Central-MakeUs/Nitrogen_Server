4b3aa2bc51efba99d1f62eafa8c76bef0a05de3b
huggywuggy1289 <153150703+huggywuggy1289@users.noreply.github.com>
2026-02-10 18:24:41 +0900

    :sparkles: Feature Add 1:1 inquiry API for App Store review

---
+++ src/main/java/com/nitrogen/domain/inquiry/controller/InquiryController.java
@@ -0,0 +1,35 @@
+package com.nitrogen.domain.inquiry.controller;
+
+import com.nitrogen.domain.inquiry.entity.InquiryRequest;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/inquiry")
+@RequiredArgsConstructor
+@Tag(name = "Inquiry", description = "1:1 문의 API (애플 심사 대응)")
+public class InquiryController {
+
+    private final JavaMailSender mailSender;
+
+    @PostMapping("/send")
+    @Operation(summary = "1:1 문의 메일 발송")
+    public ResponseEntity<String> sendInquiry(@RequestBody InquiryRequest request) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo("nitrogencharger@gmail.com"); // 관리자 이메일
+        message.setSubject("[1:1 문의] " + request.title());
+        message.setText("작성자: " + request.userEmail() + "\n\n문의 내용:\n" + request.content());
+
+        mailSender.send(message);
+
+        return ResponseEntity.ok("문의가 메일로 전달되었습니다.");
+    }
+}
