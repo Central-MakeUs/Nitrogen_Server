0ec02f6d9e6941c577e87a6f442a802116860167
huggywuggy1289 <153150703+huggywuggy1289@users.noreply.github.com>
2026-02-15 23:02:06 +0900

    :sparkles: Feature 소셜 로그인 시 중복 엔티티 조회 오류 수정 및 방어 로직 추가

--- src/main/java/com/nitrogen/domain/user/repository/UserRepository.java
+++ src/main/java/com/nitrogen/domain/user/repository/UserRepository.java
@@ -2,10 +2,15 @@ package com.nitrogen.domain.user.repository;
 
 import com.nitrogen.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    @Query("SELECT u FROM User u WHERE u.socialId = :socialId AND u.socialId IS NOT NULL")
     Optional<User> findBySocialId(String socialId);
+
+    @Query("SELECT u FROM User u WHERE u.appleSub = :appleSub AND u.appleSub IS NOT NULL")
     Optional<User> findByAppleSub(String appleSub);
 }
