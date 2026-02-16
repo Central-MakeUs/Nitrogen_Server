f451c17105e8ff09107847c01d3843e89f91815b
huggywuggy1289 <153150703+huggywuggy1289@users.noreply.github.com>
2026-02-16 10:55:54 +0900

    :sparkles: Feature 애플 로그인 및 탈퇴 로직 버그 수정

--- src/main/java/com/nitrogen/domain/user/repository/UserRepository.java
+++ src/main/java/com/nitrogen/domain/user/repository/UserRepository.java
@@ -8,9 +8,7 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    @Query("SELECT u FROM User u WHERE u.socialId = :socialId AND u.socialId IS NOT NULL")
     Optional<User> findBySocialId(String socialId);
 
-    @Query("SELECT u FROM User u WHERE u.appleSub = :appleSub AND u.appleSub IS NOT NULL")
     Optional<User> findByAppleSub(String appleSub);
 }
