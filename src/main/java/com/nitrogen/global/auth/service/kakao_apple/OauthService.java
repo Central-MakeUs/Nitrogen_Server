7ac8150925a4939390702ec08213ac47840d85a2
huggywuggy1289 <153150703+huggywuggy1289@users.noreply.github.com>
2026-02-16 10:51:20 +0900

    :sparkles: Feature 탈퇴로직 분기처리

--- src/main/java/com/nitrogen/global/auth/service/kakao_apple/OauthService.java
+++ src/main/java/com/nitrogen/global/auth/service/kakao_apple/OauthService.java
@@ -130,8 +130,6 @@ public class OauthService {
     // apple kakao 공통 로그아웃
     @Transactional
     public void withdraw(String identifier) {
-        log.info(">>>> 탈퇴 시도 identifier: [{}]", identifier);
-
         User user = userRepository.findBySocialId(identifier)
                 .orElseGet(() -> userRepository.findByAppleSub(identifier)
                         .orElseThrow(() -> new RuntimeException("해당 유저를 찾을 수 없습니다.")));
@@ -141,7 +139,6 @@ public class OauthService {
         } else if ("apple".equals(user.getProvider())) {
             unlinkApple(user.getRefreshToken());
         }
-
         userRepository.delete(user);
     }
 
