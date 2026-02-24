b1c44522c12f3fef5570d0cce4f7e0a92f74934d
huggywuggy1289 <153150703+huggywuggy1289@users.noreply.github.com>
2026-02-24 11:44:19 +0900

    :sparkles: Feature 여러 개의 정렬 기준 성립

--- src/main/java/com/nitrogen/domain/alert/repository/AlertRepository.java
+++ src/main/java/com/nitrogen/domain/alert/repository/AlertRepository.java
@@ -7,6 +7,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface AlertRepository extends JpaRepository<Alert, Long> {
-    // 특정 유저의 알림을 생성일자 기준 내림차순(최신순)으로 조회
+    // 읽지 않은 알림(false) 우선 + 그 안에서 최신순 정렬
+    List<Alert> findAllByUserOrderByIsReadAscCreatedAtDesc(User user);
+
+    // 전체 알림 최신순
     List<Alert> findAllByUserOrderByCreatedAtDesc(User user);
 }
