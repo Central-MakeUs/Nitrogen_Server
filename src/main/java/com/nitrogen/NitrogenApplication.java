474ea1ba74d811bbfb9e6169988ca3098eb00f14
huggywuggy1289 <153150703+huggywuggy1289@users.noreply.github.com>
2026-03-03 03:59:56 +0900

    :bug: Fix: 버 시간대를 한국(KST)으로 고정 및 미래 시간에 대한 예외처리 보강

--- src/main/java/com/nitrogen/NitrogenApplication.java
+++ src/main/java/com/nitrogen/NitrogenApplication.java
@@ -1,10 +1,18 @@
 package com.nitrogen;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class NitrogenApplication {
+
+    @PostConstruct
+    public void setTimeZone() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(NitrogenApplication.class, args);
